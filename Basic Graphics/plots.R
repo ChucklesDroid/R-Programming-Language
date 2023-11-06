cb55e39d00a6d9cfaaa9be0ccc966574d578eae2
Aakarsh MJ <mj.akarsh@gmail.com>
2023-11-06 18:28:51 +0530

    Added plots using plot()
    Signed-off-by: Aakarsh MJ <mj.akarsh@gmail.com>

---
+++ Basic Graphics/plots.R
@@ -0,0 +1,26 @@
+library(datasets)
+
+head(iris)
+
+plot(iris$Petal.Width)                  #Quantitative Variable
+plot(iris$Species)                      #Categorical Variable(automatically detects) # nolint
+plot(iris$Species, iris$Petal.Width)    #Box plot
+plot(iris)                              #Plot entire data frame
+
+plot(cos, 0, 2*pi)                      #Cosine curve
+plot(exp, 0, 5)                         #Exponential curve
+plot(dnorm, -5, 5)                      #Distribution Normal curve
+
+plot(iris$Petal.Length, iris$Petal.Width,
+    col = "#cc0000",
+    pch = 19,
+    main = "Scatter Plot",
+    xlab = "Petal Length",
+    ylab = "Petal Width")
+
+plot(dnorm, -5, +5,
+    col = "#cc0000",
+    lwd = 5,
+    main = "Standard Normal Distribution",
+    xlab = "z-scores",
+    ylab = "Density")
